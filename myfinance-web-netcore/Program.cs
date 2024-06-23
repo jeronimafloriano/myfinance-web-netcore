e51b9798d368fc7cba83e08352421cbd1ecfbab4
jeronima <jeronimajc@hotmail.com>
2024-06-23 16:06:47 -0300

    feat: Arquivos de configuração

--- myfinance-web-netcore/Program.cs
+++ myfinance-web-netcore/Program.cs
@@ -1,8 +1,16 @@
+using myfinance_web_netcore.Infrastructure;
+using myfinance_web_netcore.Mappers;
+using myfinance_web_netcore.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<MyFinanceDbContext>();
+builder.Services.AddAutoMapper(typeof(PlanoContaMap));
+builder.Services.AddAutoMapper(typeof(TransacaoMap));
+builder.Services.AddScoped<IPlanoContaService, PlanoContaService>();
+builder.Services.AddScoped<ITransacaoService, TransacaoService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
