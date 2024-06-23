1dc1d77d116c3a2ca088840ed0bcc82d43878621
jeronima <jeronimajc@hotmail.com>
2024-06-23 16:04:56 -0300

    feat: Conexão com banco e script sql

---
+++ myfinance-web-netcore/Infrastructure/MyFinanceDbContext.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using myfinance_web_netcore.Domain;
+
+namespace myfinance_web_netcore.Infrastructure{
+
+    public class MyFinanceDbContext: DbContext{
+        
+        public DbSet<PlanoConta> PlanoConta {get;set;}  
+        public DbSet<Transacao> Transacao {get; set;}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){
+            var connectionString = @"Server=localhost\SQLEXPRESS;Database=myfinance;Trusted_Connection=True;TrustServerCertificate=True";
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
