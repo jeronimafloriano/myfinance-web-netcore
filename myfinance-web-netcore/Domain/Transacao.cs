85bf153c1289aabd0aae5f407e84d4227b047194
jeronima <jeronimajc@hotmail.com>
2024-06-23 16:03:30 -0300

    feat: Adicionando classes de domínio

---
+++ myfinance-web-netcore/Domain/Transacao.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using myfinance_web_netcore.Models;
+
+namespace myfinance_web_netcore.Domain{
+
+    public class Transacao{
+        
+        public int Id {get; set;}
+        public string? Historico {get; set;}
+        public DateTime Data {get;set;}
+        public decimal Valor {get;set;}
+        public int PlanoContaId {get;set;}
+        public PlanoConta PlanoConta {get; set;}
+        
+    }
+}
